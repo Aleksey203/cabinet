73966cdfc3093d702ffb6928a0f1cdfce4c75269
Aleksey Berlinskiy <al.berlinskiy@ya.ru>
2015-04-30 10:34:58 +0300

    Добавление папки Vendor и префикса таблиц

--- controllers/CountryController.php
+++ controllers/CountryController.php
@@ -14,7 +14,7 @@ use yii\filters\VerbFilter;
  */
 class CountryController extends Controller
 {
-    public function behaviors()
+/*    public function behaviors()
     {
         return [
             'verbs' => [
@@ -24,7 +24,7 @@ class CountryController extends Controller
                 ],
             ],
         ];
-    }
+    }*/
 
     /**
      * Lists all Country models.
@@ -32,90 +32,15 @@ class CountryController extends Controller
      */
     public function actionIndex()
     {
-        $searchModel = new CountrySearch();
-        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+        if (\Yii::$app->request->isAjax) {
+            $data = array();
 
-        return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
-        ]);
-    }
+            $data['html'] = date('H:i:s');
 
-    /**
-     * Displays a single Country model.
-     * @param string $id
-     * @return mixed
-     */
-    public function actionView($id)
-    {
-        return $this->render('view', [
-            'model' => $this->findModel($id),
-        ]);
-    }
-
-    /**
-     * Creates a new Country model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
-    public function actionCreate()
-    {
-        $model = new Country();
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->code]);
-        } else {
-            return $this->render('create', [
-                'model' => $model,
-            ]);
+            return json_encode($data);
         }
+
+        return $this->render('index');
     }
 
-    /**
-     * Updates an existing Country model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param string $id
-     * @return mixed
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->code]);
-        } else {
-            return $this->render('update', [
-                'model' => $model,
-            ]);
-        }
-    }
-
-    /**
-     * Deletes an existing Country model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param string $id
-     * @return mixed
-     */
-    public function actionDelete($id)
-    {
-        $this->findModel($id)->delete();
-
-        return $this->redirect(['index']);
-    }
-
-    /**
-     * Finds the Country model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param string $id
-     * @return Country the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    protected function findModel($id)
-    {
-        if (($model = Country::findOne($id)) !== null) {
-            return $model;
-        } else {
-            throw new NotFoundHttpException('The requested page does not exist.');
-        }
-    }
 }
--- views/country/index.php
+++ views/country/index.php
@@ -7,30 +7,18 @@ use yii\grid\GridView;
 /* @var $searchModel app\models\CountrySearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Countries';
+$this->title = 'Рабочий стол';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="country-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <p>
-        <?= Html::a('Create Country', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+    <div id="box">
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'code',
-            'name',
-            'population',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
+    </div>
 
 </div>
+<?php
+$this->registerJsFile('/js/monitor.js',['depends'=>'app\assets\AppAsset']);
+?>
